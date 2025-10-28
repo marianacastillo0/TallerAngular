150e4f9f61172048a87f90fc8514969d19dad991
Mariana Castillo <m.castillo112345@uniandes.edu.co>
2025-10-28 10:17:21 -0500

    Subiendo proyecto Angular a GitHub

--- src/app/course/course.service.ts
+++ src/app/course/course.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Course } from '../course/course';
+
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
+  private apiUrl = environment.apiUrl + '/courses.json';
+  constructor(private http: HttpClient) { }
   
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.apiUrl);
 }
